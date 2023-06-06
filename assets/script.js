898b80b82b661b4e4ea4377ccf020f0a412c3dcb
Pavoni1978 <>
2023-06-06 14:48:41 +0000

    src updated

---
+++ assets/script.js
@@ -0,0 +1,11 @@
+var height = parseInt (document.getElementById("height").value);
+
+document.getElementById ("height-val").textContent = height + "cm";
+
+var weight = parseInt (document.getElementById ("weight").value);
+
+document.getElementById("weight-val").textContent = weight + "kg";
+
+var result = document.getElementById("result")
+
+
