2f5bfe1a390db40c9e0d25c5e6c8cd06e8038944
Pavoni1978 <>
2023-06-09 20:04:35 +0000

    code amended after speaking to tutor

--- assets/script.js
+++ assets/script.js
@@ -8,17 +8,16 @@ document.getElementById(height-val).textContent = height + "cm";
 
 let myButton = document.getElementById("btn");
 
-// you see the click will trigger the best fucntion
-
-myButton.addEventListener("click",Get My BMI
+myButton.addEventListener("click","Get My BMI"
 );
 
 function best(){
-    console.log("Get My BMI")
-    alert("Your BMI"
+    console.log("click")
+    alert("Your BMI")
 
- var result= ("Your BMI"); {
+ result= ("Your BMI"); {
      (((weight / ((height * height)
 /10000)).toFixed(2)));
+ }
 
-}
+
