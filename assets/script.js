e03190bbf9abb2c7a1a40692f4b305ab40eabf2c
Pavoni1978 <>
2023-06-09 19:53:37 +0000

    amendments after speaking to mentor

--- assets/script.js
+++ assets/script.js
@@ -2,11 +2,20 @@ var weight = parseInt (document.getElementById (weight).value);
 
 document.getElementById(weight-val).textContent = weight + "kg";
 
-var height = parseInt (document.getElementById(height).value);
+var height = parseInt(document.getElementById(height).value);
 
-document.getElementById (height-val).textContent = height + "cm";
+document.getElementById(height-val).textContent = height + "cm";
 
-document.getElementById = ("btn").addEventListener("click", displayBMI);
+let myButton = document.getElementById("btn");
+
+// you see the click will trigger the best fucntion
+
+myButton.addEventListener("click",Get My BMI
+);
+
+function best(){
+    console.log("Get My BMI")
+    alert("Your BMI")
 
  var result= ("Your BMI"); {
      (((weight / ((height * height)
